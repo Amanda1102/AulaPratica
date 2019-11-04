2d2dffd2143def260ac268fb3a0d750fa213ab73
Amanda <amandaarteixeira@hotmail.com>
2019-11-03 23:29:59 -0300

    TESTE 5

--- src/test/Money.java
+++ src/test/Money.java
@@ -2,8 +2,12 @@ package src.test;
 
 abstract class Money {
 	private String currency;
-	
+
 	protected int amount;
+
+	String currency() {
+		return currency;
+	}
 
 	static Money dollar(int amount)  {
 		return new Dollar(amount, "USD");
@@ -16,5 +20,14 @@ abstract class Money {
 	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
+	}
+
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
 	}
 }
