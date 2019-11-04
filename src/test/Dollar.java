57f8fe283b5b72f3f173b280a7b023c034f4a16f
Amanda <amandaarteixeira@hotmail.com>
2019-11-03 23:06:40 -0300

    TESTE 2

--- src/test/Dollar.java
+++ src/test/Dollar.java
@@ -1,7 +1,10 @@
 package src.test;
 
-public class Dollar{
-	int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
+public class Dollar extends Money{
+	Dollar(int amount) {
+		this.amount= amount;
+	}
+	void times(int multiplier) {
+		amount= amount * multiplier;
+	}
 }
